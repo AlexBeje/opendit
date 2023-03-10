016c6312f27cb09c9e27b4d6e8284f44b809e742
AlexBeje <alex.bejenaru93@gmail.com>
2023-03-10 12:40:25 +0100

    📦 NEW: Added click outside event util handler.

--- components/AddNew.vue
+++ components/AddNew.vue
@@ -1,34 +1,41 @@
 <template>
-  <el-button
-    class="relative"
-    type="primary"
-    color="#000"
-    round
-    @click="dialogVisible = !dialogVisible"
-  >
-    Add New<el-icon class="el-icon--right"><Plus /></el-icon>
-  </el-button>
-  <div
-    v-if="dialogVisible"
-    class="absolute bg-white drop-shadow mt-10 rounded-lg shadow-lg text-xs w-[110px] z-10"
-  >
-    <p
-      class="cursor-pointer p-3 rounded-tl-lg rounded-tr-lg text-center hover:bg-gray-200"
+  <div ref="addNewComponent" class="relative">
+    <el-button
+      type="primary"
+      color="#000"
+      round
       @click="dialogVisible = !dialogVisible"
     >
-      Add manually
-    </p>
-    <p
-      class="cursor-pointer p-3 rounded-bl-lg rounded-br-lg text-center hover:bg-gray-200"
-      @click="dialogVisible = !dialogVisible"
+      Add New<el-icon class="el-icon--right"><Plus /></el-icon>
+    </el-button>
+    <div
+      v-if="dialogVisible"
+      class="absolute bg-white drop-shadow mt-2 rounded-lg shadow-lg text-xs w-[110px] z-10"
     >
-      Import Excel
-    </p>
+      <p
+        class="cursor-pointer p-3 rounded-tl-lg rounded-tr-lg text-center hover:bg-gray-200"
+        @click="dialogVisible = !dialogVisible"
+      >
+        Add manually
+      </p>
+      <p
+        class="cursor-pointer p-3 rounded-bl-lg rounded-br-lg text-center hover:bg-gray-200"
+        @click="dialogVisible = !dialogVisible"
+      >
+        Import Excel
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Plus } from '@element-plus/icons-vue';
+import useDetectOutsideClick from '~~/utils/events';
 
 const dialogVisible = ref(false);
+const addNewComponent = ref(null);
+
+useDetectOutsideClick(addNewComponent, () => {
+  dialogVisible.value = false;
+});
 </script>
